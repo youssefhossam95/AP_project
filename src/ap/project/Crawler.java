d47cdd3e8af0c78ff509ab94536b6493d23e0eb0
youssefhossam95 <youssefhossam95@hotmail.com>
2017-03-07 20:14:19 +0200

    crawler added

---
+++ src/ap/project/Crawler.java
@@ -0,0 +1,5 @@
+package ap.project;
+
+public class Crawler {
+
+}
---
+++ src/ap/project/TestMain.java
@@ -0,0 +1,54 @@
+package ap.project;
+import org.jsoup.*;
+import org.jsoup.helper.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+public class TestMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        Validate.isTrue(args.length == 1, "usage: supply url to fetch");
+		        String url = args[0];
+		        print("Fetching %s...", url);
+
+		        Document doc = Jsoup.connect(url).get();
+		        Elements links = doc.select("a[href]");
+		        Elements media = doc.select("[src]");
+		        Elements imports = doc.select("link[href]");
+
+		        print("\nMedia: (%d)", media.size());
+		        for (Element src : media) {
+		            if (src.tagName().equals("img"))
+		                print(" * %s: <%s> %sx%s (%s)",
+		                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
+		                        trim(src.attr("alt"), 20));
+		            else
+		                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
+		        }
+
+		        print("\nImports: (%d)", imports.size());
+		        for (Element link : imports) {
+		            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
+		        }
+
+		        print("\nLinks: (%d)", links.size());
+		        for (Element link : links) {
+		            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
+		        }
+		    }
+
+		    private static void print(String msg, Object... args) {
+		        System.out.println(String.format(msg, args));
+		    }
+
+		    private static String trim(String s, int width) {
+		        if (s.length() > width)
+		            return s.substring(0, width-1) + ".";
+		        else
+		            return s;
+		    }
+		}
+		
+	}
+
+}
